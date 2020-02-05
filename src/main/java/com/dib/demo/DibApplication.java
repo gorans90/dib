551ea5e7c230e52bd3d7033cb57855fce67eff18
Goran Simic <gsimic@groundlink.com>
2020-02-05 14:22:12 +0100

    init project

---
+++ src/main/java/com/dib/demo/DibApplication.java
@@ -0,0 +1,13 @@
+package com.dib.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DibApplication.class, args);
+	}
+
+}
